cfbc8b5681a98d2ec6731f065dec9cec4f5fe581
David Addai <dkag03@gmail.com>
2017-04-17 07:47:34 -0700

    added comments

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -5,23 +5,28 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 /**
  * Created by David on 4/16/2017.
+ * This class would evntually load properties from a config file specifying things such as webdriver type (chome, firefox, etc)
+ * More helper methods to setup and teardown after tests are run,
+ * Methods for integration with other frameworks, etc
  */
 public class TestBase {
 
     private static WebDriver driver;
 
+    //constructor to initialize a chrome driver to be used by tests
     public TestBase(){
-        System.setProperty("webdriver.chrome.driver", "C:\\Programming_Home\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--incognito", "--disable-popup-blocking", "--disable-extensions", "--start-maximized");
         this.driver = new ChromeDriver(options);
     }
 
+    //helper method to get the webdriver being used
     public WebDriver getDriver(){
         return this.driver;
     }
 
-
+    //Kill browser after test
     @AfterClass
     public static void tearDown(){
         driver.quit();
